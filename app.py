7de11f1135578bd9c88aded1c8a52030ba72df73
blue <swastiksrichandan@gmail.com>
2022-08-21 11:02:44 +0530

    join game

--- app.py
+++ app.py
@@ -6,9 +6,49 @@ import json
 import websockets
 
 from connect4 import PLAYER1, PLAYER2, Connect4
+import secrets
+
+
+JOIN = {}
+
+
+async def start(websocket):
+    # Initialize a Connect Four game, the set of WebSocket connections
+    # receiving moves from this game, and secret access token.
+    game = Connect4()
+    connected = {websocket}
+
+    join_key = secrets.token_urlsafe(12)
+    JOIN[join_key] = game, connected
+
+    try:
+        # Send the secret access token to the browser of the first player,
+        # where it'll be used for building a "join" link.
+        event = {
+            "type": "init",
+            "join": join_key,
+        }
+        await websocket.send(json.dumps(event))
+
+        # Temporary - for testing.
+        print("first player started game", id(game))
+        async for message in websocket:
+            print("first player sent", message)
+
+    finally:
+        del JOIN[join_key]
 
 
 async def handler(websocket):
+
+    # Receive and parse the "init" event from the UI.
+    message = await websocket.recv()
+    event = json.loads(message)
+    assert event["type"] == "init"
+
+    # First player starts a new game.
+    await start(websocket)
+    
     # Initialize a Connect Four game.
     game = Connect4()
 
